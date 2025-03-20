49ff0a16d0a546a95b5a3b59a900bad3112ae942
Caumond Raphaël <raphael.caumond@etu.uca.fr>
2025-03-20 13:35:23 +0100

    [QUOR-0] Add basic MAUI project

---
+++ Quoridor/QuoridorMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuoridorMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
