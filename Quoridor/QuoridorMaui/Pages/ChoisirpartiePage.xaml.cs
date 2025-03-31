44d273546bf57272692aa15dd6a842cf9cf50c2b
Caumond Raphaël <raphael.caumond@etu.uca.fr>
2025-03-31 23:18:37 +0200

    [4] wip : almost finished, background image missing

---
+++ Quoridor/QuoridorMaui/Pages/ChoisirpartiePage.xaml.cs
@@ -0,0 +1,14 @@
+namespace QuoridorMaui.Pages;
+
+public partial class ChoisirpartiePage : ContentPage
+{
+	public ChoisirpartiePage()
+	{
+		InitializeComponent();
+	}
+    private void test_bouton(object sender, EventArgs e)
+    {
+        DisplayAlert("Confirmer", "coucou poulet", "ok");
+
+    }
+}
