5c51a0dc0286eea442583b6b96a3a0cd33854ea7
Caumond Raphaël <raphael.caumond@etu.uca.fr>
2025-04-01 21:42:54 +0200

    [5] wip : Parties list + menu done -> need set button list

---
+++ Quoridor/QuoridorMaui/Pages/ChoisirSauvegardePage.xaml.cs
@@ -0,0 +1,24 @@
+namespace QuoridorMaui.Pages;
+
+public partial class ChoisirSauvegardePage : ContentPage
+{
+
+
+    private void test_bouton(object sender, EventArgs e)
+    {
+        DisplayAlert("Confirmer", "coucou poulet", "ok");
+
+    }
+    bool isSelected = false;
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        isSelected = !isSelected;
+        VisualStateManager.GoToState((Button)sender, isSelected ? "Selected" : "Normal");
+    }
+
+    public ChoisirSauvegardePage()
+    {
+        InitializeComponent();
+    }
+}
