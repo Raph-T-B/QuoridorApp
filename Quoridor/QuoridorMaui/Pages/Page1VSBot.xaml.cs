6db4c6cd1936c1d042b5c12fa31490fd7ea5191c
Gabin Ferrandon <gabinferrandon@mac.home>
2025-04-06 20:21:56 +0200

    [6]MaJ1vsBotPage

---
+++ Quoridor/QuoridorMaui/Pages/Page1VSBot.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoridorMaui.Pages;
+
+public partial class Page1VSBot : ContentPage
+{
+    public Page1VSBot()
+    {
+        InitializeComponent();
+    }
+}
