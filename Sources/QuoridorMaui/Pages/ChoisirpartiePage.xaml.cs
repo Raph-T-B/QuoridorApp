36d830ac6a3d3b615494b7a7f4ebe891b195760b
Caumond Raphaël <raphael.caumond@etu.uca.fr>
2025-04-07 11:40:14 +0200

    [5] endof branch

--- Sources/QuoridorMaui/Pages/ChoisirpartiePage.xaml.cs
+++ Sources/QuoridorMaui/Pages/ChoisirpartiePage.xaml.cs
@@ -2,13 +2,13 @@ namespace QuoridorMaui.Pages;
 
 public partial class ChoisirpartiePage : ContentPage
 {
-	public ChoisirpartiePage()
-	{
-		InitializeComponent();
-	}
     private void test_bouton(object sender, EventArgs e)
     {
         DisplayAlert("Confirmer", "coucou poulet", "ok");
 
     }
+    public ChoisirpartiePage()
+	{
+		InitializeComponent();
+	}
 }
