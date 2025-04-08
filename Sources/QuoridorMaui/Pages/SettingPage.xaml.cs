8db8cf819dd64c11c6ce23883eed3f97b50fb51c
Caumond Raphaël <raphael.caumond@etu.uca.fr>
2025-04-08 22:29:34 +0200

    [18] work finished EO branch 18

---
+++ Sources/QuoridorMaui/Pages/SettingPage.xaml.cs
@@ -0,0 +1,76 @@
+namespace QuoridorMaui.Pages;
+
+public partial class SettingPage : ContentPage
+{
+    private void test_bouton(object sender, EventArgs e)
+    {
+        DisplayAlert("Confirmer", "coucou", "ok");
+
+    }
+    
+
+    private void OnThemeButtonClicked(object sender, EventArgs e)
+    {
+        if (sender == BtnSombre)
+        {
+            BtnSombre.BackgroundColor = Colors.LightSalmon;
+            BtnSombre.TextColor = Colors.White;
+
+            BtnClair.BackgroundColor = Colors.White;
+            BtnClair.TextColor = Colors.Black;
+        }
+        else if (sender == BtnClair)
+        {
+            BtnClair.BackgroundColor = Colors.LightSalmon;
+            BtnClair.TextColor = Colors.White;
+
+            BtnSombre.BackgroundColor = Colors.White;
+            BtnSombre.TextColor = Colors.Black;
+        }
+    }
+    private void OnDeplacementButtonClicked(object sender, EventArgs e)
+    {
+        if (sender == BtnShowDep)
+        {
+            BtnShowDep.BackgroundColor = Colors.LightSalmon;
+            BtnShowDep.TextColor = Colors.White;
+
+            BtnHideDep.BackgroundColor = Colors.White;
+            BtnHideDep.TextColor = Colors.Black;
+        }
+        else if (sender == BtnHideDep)
+        {
+            BtnHideDep.BackgroundColor = Colors.LightSalmon;
+            BtnHideDep.TextColor = Colors.White;
+
+            BtnShowDep.BackgroundColor = Colors.White;
+            BtnShowDep.TextColor = Colors.Black;
+        }
+    }
+
+    private void OnMurButtonClicked(object sender, EventArgs e)
+    {
+        if (sender == BtnShowWall)
+        {
+            BtnShowWall.BackgroundColor = Colors.LightSalmon;
+            BtnShowWall.TextColor = Colors.White;
+
+            BtnHideWall.BackgroundColor = Colors.White;
+            BtnHideWall.TextColor = Colors.Black;
+        }
+        else if (sender == BtnHideWall)
+        {
+            BtnHideWall.BackgroundColor = Colors.LightSalmon;
+            BtnHideWall.TextColor = Colors.White;
+
+            BtnShowWall.BackgroundColor = Colors.White;
+            BtnShowWall.TextColor = Colors.Black;
+        }
+    }
+
+
+    public SettingPage()
+	{
+		InitializeComponent();
+	}
+}
