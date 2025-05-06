382e824e1acd736ea3af1f701283e64ce93fba5a
Caumond Raphaël <raphael.caumond@etu.uca.fr>
2025-05-06 22:17:29 +0200

    [45] Wip RoundClass

--- Sources/QuoridorLib/Models/Round.cs
+++ Sources/QuoridorLib/Models/Round.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+
+
+
 namespace QuoridorLib.Models
 {
+    // pour la dependenci
+    public delegate void Progression(int pourcentage);
     
    class Round : Board
     {
 
-        // ✅ Propriété publique avec accès en lecture (get) mais écriture interne uniquement (private set)
+
         public Player CurrentPlayer;
         readonly Board Board;
         public Round(Player player,Board board)
@@ -32,19 +38,42 @@ namespace QuoridorLib.Models
             Board.MovePawn(CurrentPlayer.Name, position);
         }
 
-        public void PlacingWall(int x, int y,string orientation)
+        private static List<Position> GetWallPositions(int x, int y, string orientation)
         {
-            if (orientation == "right")
+            int x1 =x , y1 =y , x2 , y2 , x3 , y3 , x4 = x+1 , y4 = y+1;
+            if (orientation == "vertical")
             {
-
+                x2 = x; y2 = y + 1;
+                x3 = x + 1; y3 = y;
             }
-            Position position1Wall1 = new Position(x1, y1);
-            Position position2Wall1 = new Position(x2, y2);
-            Position position1Wall2 = new Position(x3 y3);
-            Position position2Wall2 = new Position(x4, y4);
+            else
+            {
+                x2 = x + 1; y2 = y;
+                x3 = x; y3 = y ;
+            }
+            Position position1 = new(x1, y1);
+            Position position2 = new(x2, y2);
+            Position position3 = new(x3, y3);
+            Position position4 = new(x4, y4);
+            List<Position> wallPositions = [position1, position2, position3, position4];
+            return wallPositions;
+        }
+
+        public bool PlacingWall(int x, int y,string orientation)
+        {
+            if (Board.IsWallONBoard(x, y, orientation)){
+                return false;
+            }
+            List<Position> wallspositions= new List<Position>();
+            wallspositions=GetWallPositions(x, y, orientation);
+            Position position1Wall1 = new(wallspositions[0]);
+            Position position2Wall1 = new(wallspositions[1]);
+            Position position1Wall2 = new(wallspositions[2]);
+            Position position2Wall2 = new(wallspositions[3]);
             Wall wall1 = new Wall(position1Wall1, position2Wall1);
             Wall wall2 = new Wall(position1Wall2, position2Wall2);
             Board.AddCoupleWall(wall1,wall2);
+            return true;
         }
     }
 
