8fefd7f05a15b2a54273e57faaf189ea9d4b95d3
Gabin Ferrandon <gabinferrandon@mac.home>
2025-05-28 11:02:32 +0200

    [79]UpdateNav

--- Sources/QuoridorMaui/Pages/ChoisirSauvegardePage.xaml.cs
+++ Sources/QuoridorMaui/Pages/ChoisirSauvegardePage.xaml.cs
@@ -2,23 +2,17 @@ namespace QuoridorMaui.Pages;
 
 public partial class ChoisirSauvegardePage : ContentPage
 {
-
-
     private async void Retour_Tapped(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("homepage");
     }
+
     bool isSelected = false;
 
     private void OnButtonClicked(object sender, EventArgs e)
     {
         isSelected = !isSelected;
         VisualStateManager.GoToState((Button)sender, isSelected ? "Selected" : "Normal");
-    }
-
-    private async void Retour_Tapped(object sender, EventArgs e)
-    {
-        await Navigation.PopAsync();
     }
 
     public ChoisirSauvegardePage()
--- Sources/QuoridorMaui/Pages/HomePage.xaml.cs
+++ Sources/QuoridorMaui/Pages/HomePage.xaml.cs
@@ -6,31 +6,6 @@ public partial class HomePage : ContentPage
     {
         await Shell.Current.GoToAsync("choisirpartiepage");
     }
-    private async void LeaderBoard_Tapped(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("leaderboardpage");
-    }
-    private async void Regles_Tapped(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("settingpage");
-    }
-    private async void Quitter_Tapped(object sender, EventArgs e)
-    {
-        await Navigation.PopAsync();
-    }
-    public HomePage()
-	{
-		InitializeComponent();
-	}
-	private void test_bouton(object sender, EventArgs e){
-		DisplayAlert("Confirmer","coucou", "ok");
-
-    }
-
-    private async void Jouer_Tapped(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("choisirpartiepage");
-    }
 
     private async void Leaderboard_Tapped(object sender, EventArgs e)
     {
@@ -51,5 +26,20 @@ public partial class HomePage : ContentPage
 #elif MACCATALYST
         UIKit.UIApplication.SharedApplication.PerformSelector(new ObjCRuntime.Selector("terminateWithSuccess"), null, 0f);
 #endif
+    }
+
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private void test_bouton(object sender, EventArgs e)
+    {
+        DisplayAlert("Confirmer", "coucou", "ok");
+    }
+
+    private async void Jouer_Tapped(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("choisirpartiepage");
     }
 }
