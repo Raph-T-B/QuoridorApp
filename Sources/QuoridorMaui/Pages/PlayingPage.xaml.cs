e4e507e9acd2293ef70be460dbc4114ef8b23a0f
Gabin Ferrandon <gabinferrandon@mac.home>
2025-05-30 14:27:46 +0200

    [86]WIP

--- Sources/QuoridorMaui/Pages/PlayingPage.xaml.cs
+++ Sources/QuoridorMaui/Pages/PlayingPage.xaml.cs
@@ -1,9 +1,14 @@
+using QuoridorMaui.Models;
+
 namespace QuoridorMaui.Pages;
 
 public partial class PlayingPage : ContentPage
 {
+	public GameBoard GameBoard { get; } = new GameBoard();
+
 	public PlayingPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 }
